811ab9a946dd079cd86fe35c6c8965028adaa25e
milk-stone <ywshero@naver.com>
2025-05-25 01:05:08 +0900

    Chore: BE_Assignment2 프로젝트 초기 설정 및 application 파일 작성
    database 연결 정보 중 은닉해야하는 내용은 .env 파일에 환경변수로 작성해놓았음.

---
+++ BE_Assignment2/src/test/java/com/example/BE_Assignment2/BeAssignment2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BE_Assignment2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeAssignment2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
