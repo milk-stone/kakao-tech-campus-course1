15ffaf9a158f48777c2f589cd57607d5f85d2e4a
milk-stone <ywshero@naver.com>
2025-05-25 18:45:38 +0900

    feat: 전역 Exception Handler 파일 작성

---
+++ BE_Assignment2/src/main/java/com/example/BE_Assignment2/exception/GlobalExceptionHandler.java
@@ -0,0 +1,20 @@
+package com.example.BE_Assignment2.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<String> handleIllegalArgumentException(IllegalArgumentException e){
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+    }
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<String> handleRuntimeException(RuntimeException e){
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
+    }
+}
